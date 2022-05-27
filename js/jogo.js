9c5b3a1b199634d4fafbcc03b65fc4f1ab438267
Paulo Roberto J. Rodrigues <paulo-rjr11@hotmail.com>
2022-05-27 18:01:01 -0300

    codificando para adicionar palavra secreta.

---
+++ js/jogo.js
@@ -0,0 +1,14 @@
+var palavraSecreta = ["alura", "curso", "java", "internet", "conhecer", "carro", "moto"];
+var letra = [];
+var sorteia;
+var chance = 0;
+var tecla = 0;
+//console.log(palavraSecreta);
+
+var comecar = document.querySelector(".bt-comecar");
+var adicionar = document.querySelector(".bt-adicionar");
+var digitar = document.querySelector(".palavra");
+var salvar = document.querySelector(".bt-salvar");
+var cancelar = document.querySelector(".bt-cancelar");
+var novo = document.querySelector(".bt-novo");
+var desistir = document.querySelector(".bt-desisti");
