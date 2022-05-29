2028ea6fdb94f9e0781da2850289b3a4fe9c2483
Paulo Roberto J. Rodrigues <paulo-rjr11@hotmail.com>
2022-05-29 18:04:50 -0300

    Codificando para adicionar palavra secreta-parte2.

--- js/jogo.js
+++ js/jogo.js
@@ -3,7 +3,7 @@ var letra = [];
 var sorteia;
 var chance = 0;
 var tecla = 0;
-//console.log(palavraSecreta);
+console.log(palavraSecreta);
 
 var comecar = document.querySelector(".bt-comecar");
 var adicionar = document.querySelector(".bt-adicionar");
@@ -12,3 +12,25 @@ var salvar = document.querySelector(".bt-salvar");
 var cancelar = document.querySelector(".bt-cancelar");
 var novo = document.querySelector(".bt-novo");
 var desistir = document.querySelector(".bt-desisti");
+
+digitar.focus();
+
+function existePalavra(){
+    if(digitar.value !== ""){
+        var toda = digitar.value.toUpperCase();
+        palavraSecreta.push(toda);
+        
+        console.log(palavraSecreta);
+        limpaPalavra();
+    }            
+}
+
+/* Limpa o texto digitado e coloca o ponto de inserção para digitar novamente. */
+function limpaPalavra(){
+    digitar.value = "";
+    digitar.focus();
+}
+
+/* Eventos de clicar nos botões. */
+salvar.addEventListener("click", existePalavra);
+cancelar.addEventListener("click", limpaPalavra);
